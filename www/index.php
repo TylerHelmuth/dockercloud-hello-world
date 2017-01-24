b98fcdce8c957cb7d9111272da433dd7b89ec937
thelmuth34 <thelmuth34@gmail.com>
2017-01-24 16:28:46 -0700

    fixed unit test

--- www/index.php
+++ www/index.php
@@ -18,7 +18,8 @@
 <body>
 	<img id="logo" src="logo.png" />
 	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
-	<p></p>
+	<p>This is a website to demonstrate CI with Docker</p>
+	<p>I just auto-built, tested, and deployed this</p>
 	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
 	$links = [];
