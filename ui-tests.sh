fa169eff77cbc3d6db0079604bc6d0ac73f6ff2f
thelmuth34 <thelmuth34@gmail.com>
2017-01-24 15:47:04 -0700

    added unit tesst

---
+++ ui-tests.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sleep 5
+if curl helloworld | grep -q '<p>This is a website to demonstrate CI with Docker</p>'; then
+  echo "UI test passed!"
+  exit 0
+else
+  echo "UI test failed!"
+  exit 1
+fi
