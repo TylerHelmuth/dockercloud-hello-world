c18b0ed805b53d3028a6b637ed45246b1c6590c8
thelmuth34 <thelmuth34@gmail.com>
2017-07-05 10:06:24 -0600

    added awesome new feature

--- www/index.php
+++ www/index.php
@@ -19,6 +19,7 @@
 	<img id="logo" src="logo.png" />
 	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
 	<p></p>
+	<p>An awesome new feature</p>
 	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
 	$links = [];
