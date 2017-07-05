89af9bbc395866ad807dddada7e9a874f263a3a5
thelmuth34 <thelmuth34@gmail.com>
2017-07-05 10:11:12 -0600

    Fixed bug found by automated tests

--- www/index.php
+++ www/index.php
@@ -18,7 +18,7 @@
 <body>
 	<img id="logo" src="logo.png" />
 	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
-	<p></p>
+	<p>This is a website to demonstrate CI with Docker</p>
 	<p>An awesome new feature</p>
 	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
