5eb02cf896bf5b9a066d5659d998aaaf6de9e272
thelmuth34 <thelmuth34@gmail.com>
2017-07-05 09:27:49 -0600

    removed p

--- www/index.php
+++ www/index.php
@@ -18,7 +18,7 @@
 <body>
 	<img id="logo" src="logo.png" />
 	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
-	<p>This is a website to demonstrate CI with Docker</p>
+	<!--<p>This is a website to demonstrate CI with Docker</p>-->
 	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
 	$links = [];
